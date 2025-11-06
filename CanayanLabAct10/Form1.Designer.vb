d002f9ecd0d56e9f4843d057436a6ce7da2a3f45
Vincent Canayan <vincentcanayan1@gmail.com>
2025-11-07 07:52:22 +0800

    fixed design

--- CanayanLabAct10/Form1.Designer.vb
+++ CanayanLabAct10/Form1.Designer.vb
@@ -27,11 +27,13 @@ Partial Class Form1
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Write = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Read
         '
-        Me.Read.Location = New System.Drawing.Point(394, 74)
+        Me.Read.Location = New System.Drawing.Point(103, 174)
         Me.Read.Name = "Read"
         Me.Read.Size = New System.Drawing.Size(75, 23)
         Me.Read.TabIndex = 0
@@ -40,7 +42,7 @@ Partial Class Form1
         '
         'Sort
         '
-        Me.Sort.Location = New System.Drawing.Point(394, 204)
+        Me.Sort.Location = New System.Drawing.Point(22, 174)
         Me.Sort.Name = "Sort"
         Me.Sort.Size = New System.Drawing.Size(75, 23)
         Me.Sort.TabIndex = 1
@@ -49,7 +51,7 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(394, 36)
+        Me.TextBox1.Location = New System.Drawing.Point(22, 18)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 2
@@ -57,34 +59,45 @@ Partial Class Form1
         'ListBox1
         '
         Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(394, 103)
+        Me.ListBox1.Location = New System.Drawing.Point(22, 73)
         Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.Size = New System.Drawing.Size(156, 95)
         Me.ListBox1.TabIndex = 3
         '
         'Write
         '
-        Me.Write.Location = New System.Drawing.Point(511, 36)
+        Me.Write.Location = New System.Drawing.Point(22, 44)
         Me.Write.Name = "Write"
         Me.Write.Size = New System.Drawing.Size(75, 23)
         Me.Write.TabIndex = 4
         Me.Write.Text = "Write"
         Me.Write.UseVisualStyleBackColor = True
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Write)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Sort)
+        Me.GroupBox1.Controls.Add(Me.Read)
+        Me.GroupBox1.Location = New System.Drawing.Point(372, 30)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(201, 212)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "sorting numbers"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Write)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Sort)
-        Me.Controls.Add(Me.Read)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -93,4 +106,5 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Write As Button
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
